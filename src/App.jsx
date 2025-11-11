c0e98555b5f94a67f308712b0d75302f641a1c35
Wasif-Naeem <wasif2408e@aptechgdn.net>
2025-11-11 23:25:18 +0500

    Folder Structure & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import Projects from './pages/Projects';
 
 function App() {
   return (
     <div className="bg-[#ffffff] text-black min-h-screen font-sans">
       <Navbar />
-      <HeroSection />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
     </div>
   );
 }
