542e4034dc1f2e78d27a8091b37ba8b483ecc356
Ahmed Mustafa <57402517+ahmed8765@users.noreply.github.com>
2025-11-16 03:17:50 +0500

    Fix import paths for Contact1 and Contact2 components
    ContactUs uodate

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Contact1 from "../components/Contactus/Contact1.jsx";
-import Contact2 from '../components/Contactus/Contact2.jsx';
+import Contact1 from "../components/ContactUs/Contact1.jsx";
+import Contact2 from '../components/ContactUs/Contact2.jsx';
 const Contactus = () => {
   return (
     <div>
