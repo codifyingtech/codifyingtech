c13cae5c63824bfa362ecffe3632860b143f6ee3
Wasif-Naeem <wasif2408e@aptechgdn.net>
2025-11-17 19:56:21 +0500

    Text Color Fix

--- src/pages/Ourteams.jsx
+++ src/pages/Ourteams.jsx
@@ -4,7 +4,7 @@ import OurTeamCard from '../components/OurTeams/OurTeamCard'
 const Ourteams = () => {
   return (
     <div className=' mt-25 flex min-h-[90lvh] flex-col items-center'>
-      <h1 className='text-center  text-cyan-800   p-5 animate__animated animate__bounceInDown   text-5xl font-semibold  '>Meet Our Team</h1>
+      <h1 className='text-center  text-black   p-5 animate__animated animate__bounceInDown   text-5xl font-bold  '>Meet Our <span className='text-[#00AEEF]'>Team</span></h1>
       <div className='w-full'>
       <OurTeamCard/>
       </div> 
